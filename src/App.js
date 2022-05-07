ece864793a7a1f46d34be332610cc041e62be190
uchenna <uchennaonwuliri@gmail.com>
2022-05-07 21:17:00 +0100

    first commit
    nothing much added just the navbar footer components

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// NOTE to use links you must import and use route and router
+import { BrowserRouter as Router,Route } from "react-router-dom";
+import Navbar from "./components/layouts/Navbar";
+import Footer from "./components/layouts/Footer";
+
+function App() {
+  return (
+    <Router>
+      <div className="flex flex-col justify-between h-screen ">
+        <Navbar/>
+          <main className="conatiner mx-auto px-3 pb-12">
+            content 
+          </main>
+        <Footer/>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
