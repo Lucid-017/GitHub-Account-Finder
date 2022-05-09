8b479dd83fea6300169345d213a9fe4b6b7403fd
uchenna <uchennaonwuliri@gmail.com>
2022-05-09 05:49:23 +0100

    added an optional .env file,updated home page
     an environment variable (.env) holds data that can be accesed from anywhere in the application,this hodes the gitHub api url and token key

--- src/components/layouts/pages/Home.jsx
+++ src/components/layouts/pages/Home.jsx
@@ -2,6 +2,9 @@ const Home = () => {
   return (
     <div>
         <h1 className="text-6xl ">Welcome</h1>
+        {/* testing the environment variable */}
+        {/* {process.env.REACT_APP_GITHUB_CODE} */}
+        
     </div>
   )
 }
