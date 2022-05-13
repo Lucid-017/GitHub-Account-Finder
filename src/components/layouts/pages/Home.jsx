426b04a44005afa11b0fccc0390c0a280dee504e
uchenna <uchennaonwuliri@gmail.com>
2022-05-13 01:10:35 +0100

    user search component
    imported user search into home.jsx, created a new user component called usersearch.jsx that contains a form with input and buttons ,this component will fetch users depending on what we pass into the submit in the form element

--- src/components/layouts/pages/Home.jsx
+++ src/components/layouts/pages/Home.jsx
@@ -1,7 +1,10 @@
 import UserResults from "../users/UserResults"
+import UserSearch from "../users/UserSearch"
+
 const Home = () => {
   return (
     <>
+        <UserSearch/>
         <UserResults/>
         {/* testing the environment variable */}
         {/* {process.env.REACT_APP_GITHUB_CODE} */}
