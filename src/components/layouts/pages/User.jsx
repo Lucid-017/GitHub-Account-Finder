07390ca1877476e3b210c579b8a8a17782cbc22c
Lucid <67615874+Lucid-017@users.noreply.github.com>
2022-06-11 01:33:54 +0100

    Cleaning Up Our Actions & Axios
    install axios and replace the fetch method,make get users and get user repo one function in the component and reducer

--- src/components/layouts/pages/User.jsx
+++ src/components/layouts/pages/User.jsx
@@ -3,21 +3,20 @@ import {FaCodepen , FaStore, FaUserFriends, FaUsers} from 'react-icons/fa'
 import GIthubContext from "../../../context/github/GIthubContext"
 import { Link } from "react-router-dom"
 import Spinner from "../Spinner"
-import {getUserRepo,getUser} from '.././../../context/GithubActions'
+import {getuserAndRepos} from '.././../../context/GithubActions'
 
 import { useParams } from "react-router-dom"
 import RepoList from "../Repo/RepoList"
+
 const User = () => {
     const {user,repos,dispatch, loading}= useContext(GIthubContext)
     const params = useParams()
     useEffect(()=>{
         dispatch({type:'SET_LOADING'})
         const getUserData =async ()=>{
-          const userData = await  getUser(params.login)
-          dispatch({type:'GET_USER', payload:userData})
+          const userData = await  getuserAndRepos(params.login)
+          dispatch({type:'GET_USER_AND_REPOS', payload:userData})
 
-          const userRepo = await  getUserRepo(params.login)
-          dispatch({type:'GET_USER_REPO', payload:userRepo})
         } 
         
         getUserData()
@@ -39,6 +38,8 @@ const User = () => {
     if(loading){
         return <Spinner/>
     }
+    // check if user website is valid
+    const userWebsite = blog?.startsWith('http') ? blog : 'https://' + blog
   return (
       <>
         <div className="w-full mx-auto lg:w-10/12">
@@ -47,22 +48,21 @@ const User = () => {
                     Back To Search 
                 </Link>
             </div>
-            <div className="grid grid-cols-1 xl:grid-cols-3
-            lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8">
+            <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8">
                 <div className="custom-card-image mb-6  md:mb-0">
                     <div className="rounded-lg shadow-xl card image-full">
                         <figure>
                             <img src={avatar_url} alt='image' />
                         </figure>
-                        <div className=" card justify-end p-8">
+                        <div className=" card-body justify-end">
                              <h2 className="card-title mb-0">
                                 {name} 
                              </h2>
-                             <p>{login}</p>
+                             <p className="flex-grow-0">{login}</p>
                         </div>
                     </div>
                 </div>
-
+                
                 <div className="col-span-2">
                     <div className="mb-6">
                         <h1 className="text-3xl card-title">
@@ -82,45 +82,56 @@ const User = () => {
                             </a>
                         </div>
                     </div>
-                    <div className="w-full rounded-lg shadow-md bg-base-100 stats">
-                        {location && (
-                            <div className="stat">
-                                <div className="stat-title text-md">
-                                    Location
-                                </div>
-                                <div className="text-lg stat-value">
-                                    {location}
-                                </div>
-                            </div>
+                    <div className=" shadow-md bg-base-100 stats">
+                    <div className=" grid grid-cols-1 md:grid-cols-2 overflow-x-hidden ">
+                    {location && (
+                           
+                             <div className="stat">
+                             <div className="stat-title pr-2">
+                                 Location
+                             </div>
+                             <div className="stat-value pr-2 text-lg">
+                             {location}
+                             </div>
+ 
+                             </div>
+                            
                         )}
                         {blog && (
+                            
                             <div className="stat">
-                                <div className="stat-title text-md">
-                                    Website
-                                </div>
-                                <div className="text-lg stat-value">
-                                    <a href={`https://${blog}`} target='_blank' rel="noreferrer">{blog}</a>
-                                </div>
+                            <div className="stat-title pr-2">
+                                Website
                             </div>
+                            <div className="stat-value pr-2 text-lg">
+                            <a href={userWebsite} target='_blank' rel="noreferrer">{blog}</a>
+                            </div>
+
+                        </div>
                         )}
                         {twitter_username && (
                             <div className="stat">
-                                <div className="stat-title text-md">
-                                    Twitter
-                                </div>
-                                <div className="text-lg stat-value">
-                                    <a href={`https://twitter.com/${twitter_username}`} target='_blank' rel="noreferrer">
-                                        {twitter_username}
-                                    </a>
-                                </div>
+                            <div className="stat-title pr-2">
+                                Twitter
                             </div>
+                            <div className="stat-value pr-2 text-lg">
+                                <a href={`https://twitter.com/${twitter_username}`} target='_blank' rel="noreferrer">
+                                                {twitter_username}
+                                            </a>
+                            </div>
+
+                        </div>
                         )}
+                        
+                    </div>
+
                     </div>
                 </div>
 
             </div>
             <div className="w-full py-5 mb-6
              rounded-lg shadow-md bg-base-100 stats">
+                 <div className="grid grid-cols-1 md:grid-cols-3">
                  <div className="stat">
                      <div className="stat-figure text-secondary">
                         <FaUsers className="text-3xl md:text-5xl"/>
@@ -173,6 +184,8 @@ const User = () => {
                      </div>
 
                  </div>
+                 </div>
+                 
             </div>
             <RepoList repos={repos}/>
         </div>
--- src/context/GithubActions.js
+++ src/context/GithubActions.js
@@ -1,64 +1,35 @@
+import axios from "axios"
+
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_CODE
 
+// creating an instance of axious that we can use
+const github = axios.create({
+    baseURL: GITHUB_URL,
+    headers:{Authorization: `${GITHUB_TOKEN}`}
+})
    //SECTION Search users
    export const searchUsers = async (text) =>{
     const params = new URLSearchParams({
         q:text,
     })
 
-const response = await fetch(`${GITHUB_URL}/search/users?${params}`,{
-    headers:{
-        Authorization: `${GITHUB_TOKEN}`
-    }
-})
-// we want to destructure the response object and get the items array
-const {items} = await response.json()
+    const response = await github.get(`/search/users?${params}`)
+// with axios we no longer need to await the response
 
-// now we are dispatching the type get users and sending data as payload
-return items
+    return response.data.items
 }  
 
-// we need a function that makes a request to gete a specific user
-// then call an action in our reducer to add new user to the state
-    //SECTION get single user
-  export  const getUser = async (login) =>{
-        
-    
-        const response = await fetch(`${GITHUB_URL}/users/${login}`,{
-            headers:{
-                Authorization: `${GITHUB_TOKEN}`
-            }
-        })
-        // if the user doesnt exist
-        if(response.status ===404){
-            window.location = '/notfound'
-        }else{
-             const data = await response.json()
-        
-        // now we are dispatching the type get single user and sending data as payload
-        return data
-        }
-        
-    }  
-    // we need a function that makes a request to get a specific user repo
-    // then call an action in our reducer to add new user to the state
-        //SECTION  get single user repo
-       export const getUserRepo = async (login) =>{
-            
-        
-        const params = new URLSearchParams({
-            sort:'created',
-            per_page:10,
-        })
-        const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`,{
-            headers:{
-                Authorization: `${GITHUB_TOKEN}`
-            }
-        })
-             const data = await response.json()
-        
-        return data
-        
-        
-    }  
+// getuser and repos
+export const getuserAndRepos = async(login)=>{
+    // we are making a call for two functions to get the user and repo
+    // we can use Promise.all which takes in an array of request
+    const [user,repos] = await Promise.all([
+        github.get(`/users/${login}`),
+        github.get(`/users/${login}/repos`)
+    ])
+
+    // since we dont have to wait for a response
+    // we can go ahead and deconstruct the user and repo object and get their data
+    return {user : user.data,repos:repos.data}
+}
